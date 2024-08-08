4f66af6bc58e05ee19ead8cf97c23cfdc000dc61
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-08 09:06:00 +0300

    clean the code

--- iterationMethodsPt2.js
+++ iterationMethodsPt2.js
@@ -1,10 +1,13 @@
 const temperatures = [22, 25, 19, 24, 28, 30, 21, 20, 27, 29, 23, 26, 24, 22, 18, 19, 21, 28, 30, 27, 26, 25, 22, 23, 19, 20, 28, 29, 27, 26];
-const  tempsGreaterThan25 = temperatures.filter((temp)=>temp>=25);
- const tempsLessThan20 = temperatures.filter((temp)=>temp<20);
 
-const tempsInFer = temperatures.map((temp)=>{ 
-   return (temp * 9/5) + 32;
-});
+const  tempsGreaterThan25 = temperatures.filter((temp)=>temp>=25);
+const tempsLessThan20 = temperatures.filter((temp)=>temp<20);
+console.log("Temparutare greater Than 25: ", tempsGreaterThan25)
+console.log("Temparutares less than 20: ", tempsLessThan20)
+
+
+const tempsInFer = temperatures.map((temp)=>(temp * 9/5) + 32);
+console.log("Temparutares in Fehrenhite: ", tempsInFer)
 
 const labeledTemps = temperatures.map((temp)=>{
 	if(temp >= 25){
@@ -15,15 +18,17 @@ const labeledTemps = temperatures.map((temp)=>{
 	return "Cool";
 	}
 });
+console.log("Labeled Temperatures: ", labeledTemps)
+
 
 let highTemp = temperatures[0];
+let lowTemp = temperatures[0];
+
 const highestTemp = temperatures.forEach((temp)=> {
 	if(temp > highTemp){
 	   highTemp = temp;
 	}
 });
-
-let lowTemp = temperatures[0];
 
 const lowestTemp = temperatures.forEach((temp)=> {
      if(temp < lowTemp){
@@ -32,12 +37,5 @@ const lowestTemp = temperatures.forEach((temp)=> {
 });
 
 
-// const LowestTemp = temeratures.reduce((temp)=>{
-//         let lowTemp = temp;
-//         if(temp > HighTemp){
-//         HighTemp = temp;
-// }
-// return lowTemp;
-// });
-
-console.log(highTemp)
+console.log("Highest Temerature = ", highTemp)
+console.log("Lowest Temerature = ", lowTemp)
